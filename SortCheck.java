37eb788a4065bad8e22845af3751e501f662dff4
carolchen1120 <72897491+carolchen1120@users.noreply.github.com>
2020-12-20 18:58:26 -0500

    Add files via upload
    I added a simple test case for the selectionSort. I will now create a tester for selectionSort that uses Random.

--- SortCheck.java
+++ SortCheck.java
@@ -17,15 +17,44 @@ public class SortCheck{
     }
   }
 
-  public static void main(String[] arr){
-    Random rand = new Random();
-    int size = rand.nextInt(100);
-    int[] data = new int[size];
-    int[] answer = new int[size];
-    for (int i = 0; i < size; i++) {
-      data[i] = rand.nextInt(10);
+
+  public static void selectionSort(int[] data){
+    int length = data.length;
+    int orig = 0;
+    int smallest = 0;
+    int smolIndex = 0;
+    for (int i = 0; i < length-1; i++) {
+      orig = data[i];
+      smallest = data[i];
+      smolIndex = i;
+      for (int j = i+1; j < length; j++) {
+        if (data[j] < smallest) {
+          smallest = data[j];
+          smolIndex = j;
+        }
+      }
+      data[i] = smallest;
+      data[smolIndex] = orig;
     }
-    bubbleSort(data);
+  }
+
+
+  public static void main(String[] arr){
+    // Random rand = new Random();
+    // int size = rand.nextInt(100);
+    // int[] data = new int[size];
+    // int[] answer = new int[size];
+    // for (int i = 0; i < size; i++) {
+    //   data[i] = rand.nextInt(10);
+    // }
+    // bubbleSort(data);
+    // answer = data;
+    // Arrays.sort(data);
+    // System.out.println(Arrays.equals(answer, data));
+
+    int[] data = {64, 25, 12, 22, 11};
+    int[] answer = new int[5];
+    selectionSort(data);
     answer = data;
     Arrays.sort(data);
     System.out.println(Arrays.equals(answer, data));
